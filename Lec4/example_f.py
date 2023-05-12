5be74b2f2d60f20633597ad6c6707a19d2df7e5a
EvgenyVl <evgeny_vlasov@yahoo.com>
2023-05-12 13:30:07 +0300

    Lec upld

--- Lec4/example_f.py
+++ Lec4/example_f.py
@@ -14,10 +14,13 @@ age = int(input())
 
 if age <= 14: # Читается сверху-вниз, первый правильный и будет выполнен
     print("Age <= 14")
+    print("Не пускаем в приложение вообще")
 elif age >= 15 and age <= 18:
     print("Age in [15, 18]")
+    print("Даем доступ к демо-версии")
 elif age > 18 and age <= 45:
     print('Age in [19, 45]')
+    print("Полный доступ к приложению")
 else: # необязательный блок
     print("Age is large")
 
