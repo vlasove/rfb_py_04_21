03b96713bbdb0df68d81fe480f3e43c96c82ffae
EvgenyVl <evgeny_vlasov@yahoo.com>
2023-05-15 13:29:02 +0300

    Lec4 done

--- Lec4/example_h.py
+++ Lec4/example_h.py
@@ -28,12 +28,24 @@ else:
 # Обсчет приоритетов при вычислении логического выражения
 
 age = int(input())
+ 
+
+# A and B
+# True and True => True
+# False and True => False
+# False and False => False
+# True and False = > False
 
 if age >= 18 and 1/0: # Если левая часть False - правая не вычисляется
     print("Позитивный случай с приоритетом and")
 else:
     print("Альтернативный случай с приоритетом and")
 
+# A or B
+# True or True => True
+# True or False => True
+# False or True => True
+# False or False => False
 
 if age >= 18 or 1/0: # Если левая часть True - правая не вычисляется
     print("Позитивный случай с приоритетом or")
