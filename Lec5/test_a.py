48158e108862cdc7a187b4bb7b7c888952d5f7af
EvgenyVl <evgeny_vlasov@yahoo.com>
2023-05-15 14:22:31 +0300

    Lec5 upld

---
+++ Lec5/test_a.py
@@ -0,0 +1,14 @@
+# Решение задачи G
+# while True:
+#     num = int(input())
+#     if num == 0:
+#         break
+#     print(num)
+
+
+# Решение задачи H
+while True:
+    msg = input()
+    if msg == "":
+        break
+    print(msg)
